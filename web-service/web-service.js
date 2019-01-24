7453446852394f45d685bebbca7abddf8852b3c8
didil <1284255+didil@users.noreply.github.com>
2019-01-24 15:58:22 +0700

    webservice

---
+++ web-service/web-service.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const amqp = require('amqplib');
+
+// Middleware
+app.use(bodyParser.json());
+
+// simulate request ids
+let lastRequestId = 1;
+
+// rabbitMQ connection string with our demo parameters (this should not be in the code for a production app)
+const messageQueueConnectionString = "amqp://appuser:123456@localhost:5672/async-microservices-demo";
+
+// handle the request
+app.post('/api/v1/processData', async function (req, res) {
+  // save request id and increment
+  let requestId = lastRequestId;
+  lastRequestId++;
+
+  // connect to Rabbit MQ and create a channel
+  let connection = await amqp.connect(messageQueueConnectionString);
+  let channel = await connection.createConfirmChannel();
+
+  // publish the data to Rabbit MQ
+  let data = req.body;
+  await publishToChannel(channel, { routingKey: "request", exchangeName: "processing", data });
+
+  // send the request id in the response
+  res.send({ requestId })
+});
+
+// function to publish a message to a channel
+function publishToChannel(channel, { routingKey, exchangeName, data }) {
+  return new Promise((resolve, reject) => {
+    channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data), 'utf-8'), { persistent: true }, function (err, ok) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve();
+    })
+  });
+}
+
+// Start the server
+const PORT = 3000;
+server = http.createServer(app);
+server.listen(PORT, "localhost", function (err) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.info("Listening on port %s.", PORT);
+  }
+});
